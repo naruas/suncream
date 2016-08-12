0ddc14280bf9a560954daecb8b9be79f9888e062
nauras <circlepie@gmal.com>
2016-08-12 11:13:50 +0900

    update

--- asset/js/suncream.js
+++ asset/js/suncream.js
@@ -100,8 +100,28 @@ Suncream.UiManager = (function(){
 			}
 		},
 		
-		/* mobile navigation toggle
+		/* file upload
 		----------------------------------------------------------*/
+		setFileUpload: function(obj){
+			$this = $(obj);
+			var $fileTarget = $this.find('.sc-upload-hidden');
+			
+			$fileTarget.on('change', function(){  // 값이 변경되면
+			    if(window.FileReader){  // modern browser
+			      var filename = $(this)[0].files[0].name;
+			    } 
+			    else {  // old IE
+			      var filename = $(this).val().split('/').pop().split('\\').pop();  // 파일명만 추출
+			    }
+			    
+			    // 추출한 파일명 삽입
+			    $this.find('.sc-upload-name').val(filename);
+			 
+			  });
+			
+		},
+		
+		
 		
 	}
 })();
